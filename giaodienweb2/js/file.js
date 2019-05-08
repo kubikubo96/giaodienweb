5f6963e3067647049982bf869b989622e07af665
KubiKubo <tien.nguyentat.1@gmail.com>
2019-05-09 02:08:29 +0700

    giao dien web

---
+++ giaodienweb2/js/file.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+    $(".dota").click(function () {
+        // $(".header").css("background-image","url(\"file:///C:/xampp/htdocs/giaodienweb/giaodienweb2/images/anhbia1_01.jpg\")");
+        $(".header").css("background-image","url(\"http://localhost:63342/giaodienweb/giaodienweb2/images/anhbia1_01.jpg\")")
+        $(".dota").css("background-color","#fff");
+        $(".dotb").css("background-color","#bbb");
+        $(".dotc").css("background-color","#bbb");
+    });
+    $(".dotb").click(function () {
+        // $(".header").css("background-image","url(\"file:///C:/xampp/htdocs/giaodienweb/giaodienweb2/images/anhbia2_01.jpg\")");
+        $(".header").css("background-image","url(\"http://localhost:63342/giaodienweb/giaodienweb2/images/anhbia2_01.jpg\")")
+
+        $(".dota").css("background-color","#bbb");
+        $(".dotb").css("background-color","#fff");
+        $(".dotc").css("background-color","#bbb");
+    });
+    $(".dotc").click(function () {
+        // $(".header").css("background-image","url(\"file:///C:/xampp/htdocs/giaodienweb/giaodienweb2/images/anhbia3_03.jpg\")");
+        $(".header").css("background-image","url(\"http://localhost:63342/giaodienweb/giaodienweb2/images/anhbia3_03.jpg\")")
+        $(".dota").css("background-color","#bbb");
+        $(".dotb").css("background-color","#bbb");
+        $(".dotc").css("background-color","#fff");
+    });
+    // alert($(".header").css("background-image"));
+});
