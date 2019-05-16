ae53a9a62ba277eb36b994ef2d4ec5f36b5e9654
KubiKubo <tien.nguyentat.1@gmail.com>
2019-05-16 16:43:52 +0700

    giao dien web

--- giaodienweb3/scripts/flatmenu-ie6.js
+++
@@ -1,9 +0,0 @@
-$(function(){		   	
-	$('ul.dropdown li').hover(function(){			
-		$(this).addClass("hover");
-		$('ul:first',this).css('display', 'block');
-	}, function(){
-		$(this).removeClass("hover");
-		$('ul:first',this).css('display', 'none');
-	});	
-});
